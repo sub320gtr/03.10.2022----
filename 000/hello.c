0fcb900f1395522c81058aab4d8971ebbda3b82c
Макс <sub320gtr@gmail.com>
2022-10-03 16:19:30 +0300

    написали helloworld!

---
+++ 000/hello.c
@@ -0,0 +1,16 @@
+/***
+\authors Титов Максим <sub320gtr@gmail.com>
+\version 0.0.1a
+\date 03.10.22
+\copuright MIT
+*/
+
+///Подключаем стандартую библиотеку
+#include <stdio.h>
+
+int hello(void) {
+    /// Выводим в стандартный вывод надпись \
+    Hello World!!! (левый слеш, перенос на другую строку)
+    printf("Hello world!!!\n");
+    return 0;
+}
---
+++ 000/main.c
@@ -0,0 +1,15 @@
+/***
+\authors Титов Максим <sub320gtr@gmail.com>
+\version 0.0.1a
+\date 03.10.22
+\copuright MIT
+\ ldd показывает динамически связанные библиотеки с файлом ldd ./hello.exe
+*/
+#include "hello.h"
+
+//Объявляем функцию main
+int main (void) {
+
+    return hello();
+    /// Возвращещаем результат функции
+}
